3c2a145341808df34e6087ad172e4fd77de44946
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-19 04:26:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,10 +3,16 @@
 def game_hash
   {
     home: {
-      team_name: '', color: [], players: []
+      team_name: '', 
+      color: [], 
+      players: []
     },
     away: {
-      team_name: '', color: [], players: []
+      team_name: '',
+      color: [],
+      players: [
+        {player_name: '' number: '', shoe: '',points: '', rebounds: '', assists: '', steals: '', blocks: '', slam_dunks: ''},
+      ]
     }
   }
 end
