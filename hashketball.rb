b991a49d0a676f579cfa82e9256f0fbbd7c06916
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-19 04:29:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     home: {
       team_name: 'Brooklyn Nets',
       color: ['Black','White'],
-      players: [{player_name: 'Alan Anderson' number: '', shoe: '',points: '', rebounds: '', assists: '', steals: '', blocks: '', slam_dunks: ''}]
+      players: [{player_name: 'Alan Anderson' number: 0, shoe: 16,points: '', rebounds: '', assists: '', steals: '', blocks: '', slam_dunks: ''}]
     },
     away: {
       team_name: '',
